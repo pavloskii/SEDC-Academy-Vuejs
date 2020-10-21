6c8a170982915e46e9d6afb0410ef02f22c5fb14
Igor Pavloski <Igor.Pavloski@seavus.com>
2020-10-21 18:40:30 +0200

    Class2-v-bind

--- Class1/index.js
+++ Class1/index.js
@@ -1,3 +1,24 @@
-// document.querySelector("#title").innerText = "Nesto drugo"
-// $("#title").text("nesto treto");
+const firstApplication = {
+  data() {
+    return {
+      subTitle: "Introduction to Vuejs",
+      classNumber: 1,
+      courses: ["HTML", "CSS", "JS", "Vuejs"],
+      showParagraph: true,
+    };
+  },
+  methods: {
+    changeSubtitle() {
+      this.subTitle = "Vuejs Events";
+    },
+    reverseString(someString) {
+      const reversed = someString.split("").reverse().join("");
+      return reversed;
+    },
+    onSubtitleInput(e) {
+      this.subTitle = e.target.value;
+    },
+  },
+};
 
+Vue.createApp(firstApplication).mount("#class1");
