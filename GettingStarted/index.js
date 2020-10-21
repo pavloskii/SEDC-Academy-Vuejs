ad4196450496e38460115e19c24dac280485f273
Igor Pavloski <Igor.Pavloski@seavus.com>
2020-10-21 20:54:54 +0200

    class 2 computed

--- GettingStarted/index.js
+++ GettingStarted/index.js
@@ -5,12 +5,16 @@ const firstApplication = {
       classNumber: 1,
       courses: ["CSS", "JS", "Vuejs", "C#"],
       showParagraph: true,
-      fontColor: "red"
+      fontColor: "red",
+      firstName: "Igor",
+      lastName: "Pavloski",
+      fullName: "Igor Pavloski",
     };
   },
   methods: {
     changeSubtitle() {
       this.subTitle = "Vuejs Events";
+      // this.subTitle = this.reverseString("Alooo")
     },
     reverseString(someString) {
       const reversed = someString.split("").reverse().join("");
@@ -18,6 +22,20 @@ const firstApplication = {
     },
     onSubtitleInput(e) {
       this.subTitle = e.target.value;
+    },
+  },
+  watch: {
+    firstName(newFirstName) {
+      this.fullName = newFirstName + " " + this.lastName;
+    },
+    lastName(newLastName) {
+      this.fullName = this.firstName + " " + newLastName;
+    },
+  },
+  computed: {
+    computedFullName() {
+      const fullName = `${this.firstName} ${this.lastName}`;
+      return fullName;
     },
   },
 };
